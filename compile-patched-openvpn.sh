2e6f954e7b1a402c02583b321b3c40dce1f6b99f
Ben V. Brown <Ralim@Ralimtek.com>
2022-11-09 13:18:43 +1100

    move to newer openvpn

--- compile-patched-openvpn.sh
+++ compile-patched-openvpn.sh
@@ -2,14 +2,14 @@
 set -e
 
 mkdir -p openvpncompile
-curl "https://swupdate.openvpn.org/community/releases/openvpn-2.4.9.tar.gz" -o openvpncompile/openvpn-2.4.9.tar.gz
+curl "https://swupdate.openvpn.org/community/releases/openvpn-2.5.1.tar.xz" -o openvpncompile/openvpn-2.5.1.tar.xz
 cd openvpncompile
-tar -zxvf openvpn-2.4.9.tar.gz
-rm -rf openvpncompile/openvpn-2.4.9.tar.gz
-cd openvpn-2.4.9
-patch -p1 <../../openvpn-v2.4.9-aws.patch
+tar -xf openvpn-2.5.1.tar.xz
+rm -rf openvpncompile/openvpn-2.5.1.tar.xz
+cd openvpn-2.5.1
+patch -p1 <../../openvpn-v2.5.1-aws.patch
 ./configure
 make -j
 cd ../..
-cp openvpncompile/openvpn-2.4.9/src/openvpn/openvpn ./openvpn-patched
+cp openvpncompile/openvpn-2.5.1/src/openvpn/openvpn ./openvpn-patched
 rm -rf openvpncompile
